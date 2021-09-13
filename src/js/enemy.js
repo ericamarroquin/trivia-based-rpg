6bf11e4f3557e71c8cda6a49efeba640bd724dfb
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-13 14:33:07 -0700

    create player class and extends create enemy class, question class, questions fetch
    Co-authored by Ben Dunham <bendunhampdx@gmail.com>
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

---
+++ src/js/enemy.js
@@ -0,0 +1,47 @@
+class Question {
+  constructor(question, correctAnswer, allAnswers) {
+    this.question = question;
+    this.correctAnswer = correctAnswer;
+    this.allAnswers = allAnswers;
+  }
+}
+
+class Enemy {
+  constructor(name, iq, attack, difficulty, category, items, amount) {
+    this.name = name;
+    this.iq = iq;
+    this.attack = attack;
+    this.difficulty = difficulty; 
+    this.category = category;
+    this.items = items;
+    this.amount = amount;
+    this.questions = [];
+  }
+
+  getQuestions() {
+    return fetch(`https://opentdb.com/api.php?amount=${this.amount}&category=${this.category}&difficulty=${this.difficulty}&type=multiple`)
+        .then(function(response) {
+          if(!response.ok){
+            throw Error(response.status);
+          }
+          return response.json();
+        })
+        .catch(function(error) {
+          return error;
+        })
+  }
+  getQ() {
+    let test = this;
+    let qArray = this.getQuestions()
+    .then(function(qArray) {
+        for (let i = 0; i < qArray.results.length; i++) {
+        let question = qArray.results[i].question;
+        let correctA = qArray.results[i].correct_answer;
+        let qAnswers = [qArray.results[i].correct_answer, qArray.results[i].incorrect_answers[0], qArray.results[i].incorrect_answers[1], qArray.results[i].incorrect_answers[2]];
+        let questionObj = new Question(question, correctA, qAnswers);
+        test.questions.push(questionObj);
+      }
+    });
+  }
+}
+
---
+++ src/js/player.js
@@ -0,0 +1,57 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.iq = 100;
+    this.exp = 0;
+    this.level = 0;
+    this.items = [];
+  }
+}
+
+//tank
+export class Professor extends Player {
+  constructor(name){
+    super(name);
+    this.iq = 110;
+  }
+}
+
+export class LiberalArts extends Player {
+  constructor(name) {
+    super(name);
+    this.exp = 10;
+    // unique ability "coffee" that lets them take one extra turn
+  }
+}
+
+export class DramaMajor extends Player {
+  constructor(name) {
+    super(name);
+    this.exp = 10;
+    //unique "burst into song" ability; confuses enemy and makes damage taken= 0 for one turn
+  }
+}
+
+export class Jock extends Player {
+  constructor(name) {
+    super(name);
+    this.iq= 90;
+    //unique ability "get stoked" causes player to do more damage for one turn
+  }
+}
+
+export class Engineer extends Player {
+  constructor(name) {
+    super(name);
+    this.iq = 110;
+    // unique ability "all nighter" will eliminate two of the wrong answers to multiple choice questions
+  }
+}
+
+export class GreekLife extends Player {
+  constructor(name) {
+    super(name);
+    this.iq= 90;
+    //unique ability "phone bro(sette)" that allows player to skip a turn and get one free answer.
+  }
+}
