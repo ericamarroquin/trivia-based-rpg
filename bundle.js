d18076aa6b4dd0c9648c12a379c95fc31bccd621
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-16 12:00:40 -0700

    Updates

--- bundle.js
+++ bundle.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "/trivia-based-rpg/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
